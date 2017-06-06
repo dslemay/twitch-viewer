bedad6c935759074bf272e2ad2c55c2981cc4319
Daniel Lemay <d.s.lemay@icloud.com>
2017-06-05 21:45:53 -0700

    Add autopopulate functionality of user data
    After the usersObj is populated it now calls another function to
    populate the visual HTML for each user. There are multiple side
    functions called within the displayUsers function that show, hide,
    or alter visual data displayed based off of some basic logic run on
    various parts of the user data.
    
    TODO:
    
    * Function for toggling which users are shown still needs to be
    updated with change of direction.
    * Final styling of user cards and colors to be completed.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -18,10 +18,11 @@ function populateObj() {
     let currentUser = twitchUsers[i];
     $.get("http://wind-bow.glitch.me/twitch-api/streams/" + twitchUsers[i], function(data) {
       usersObj[currentUser] = {};
+      usersObj[currentUser].username = currentUser;
       if (data.stream == null) {
-        usersObj[currentUser].online = false;
+        usersObj[currentUser].online = "offline";
       } else if (data.stream.hasOwnProperty("_id")) { //Tests if the user is streaming
-        usersObj[currentUser].online = true;
+        usersObj[currentUser].online = "online";
         usersObj[currentUser].game = data.stream.game;
       }
       $.get("http://wind-bow.glitch.me/twitch-api/users/" + currentUser, function(data) {
@@ -42,10 +43,65 @@ function populateObj() {
       });
     });
   }
+  setTimeout(displayUsers, 500)
 }
 
-function displayUsers() {
-  
+function displayUsers() { // Populates interface with the data stored in usersObj
+  for (var user in usersObj) {
+    let logo = logoVerify(usersObj[user].logo);
+    let game = gameVerify(usersObj[user].game);
+    let online = onlineStatus(usersObj[user].online);
+    let link = accountLink(usersObj[user].game, usersObj[user].username);
+    $('.twitch-viewer').append('<div class="twitch-user ' + usersObj[user].online + '">\
+      <div class="twitch-avatar-container">\
+        ' + logo + '\
+        </div>\
+      <div>\
+        <p class="twitch-username">' + usersObj[user].username + '</p>\
+        <p class="twitch-game">' + game + '</p>\
+      </div>\
+      <div class="icons">\
+         ' + online + '\
+        ' + link + '\
+      </div>\
+    </div>');
+  }
+}
+
+function logoVerify(logoValue) {  // Verifies that logo does not contain null or undefined and returns HTML to populate twitch-user
+  var image = "";
+  if (logoValue == null || logoValue == undefined) {
+    return image = '<img class="twitch-avatar img-placeholder" />';
+  } else {
+    return image = '<img class="twitch-avatar" src=' + logoValue + ' />'
+  }
+}
+
+function gameVerify(gameValue) { // Removes undefined values to not display visually.
+  var game = "";
+  if (gameValue == undefined) {
+    return game = "Offline";
+  } else {
+    return game = gameValue;
+  }
+}
+
+function onlineStatus(onlineValue) { // Changes the color of the icon for each account based off their onlien status
+  var online = "";
+  if (onlineValue == "online") {
+    return online = '<img src="/assets/img/online.png" class="online-icon" />'
+  } else {
+    return online = '<img src="/assets/img/offline.png" class="online-icon" />'
+  }
+}
+
+function accountLink(game, username) { // Sets link of camera icon to account page after filtering out accounts which don't exist
+  var link = "";
+  if (game == "Account does not exist" || game == "Account is unavailable") {
+    return link;
+  } else {
+    return link = '<a href="https://www.twitch.tv/' + username + '" target="_blank"><i class="fa fa-2x fa-video-camera"></i></a>';
+  }
 }
 
 
