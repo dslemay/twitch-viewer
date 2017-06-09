60864cc3d3fb0e850b494676bf1eefe7e763e533
Daniel Lemay <d.s.lemay@icloud.com>
2017-06-08 20:42:39 -0700

    Add interval update functionality
    The Twitch viewer will now poll the API every 5 minutes and update
    the object and visible data with any changes to each users online
    status and game. In order to reduce code and unnecessary API calls,
    users who do not exist or whose account is unavailable are excluded
    from this API call.
    
    Additionally, some of the code for the initial object creation was
    cleaned up. All users now have a game property created. If they are not
    streaming it is set to "Offline." Users who do not exist or are
    unavailable later overwrite this value. By manually coding in this
    property, it allowed the removal of a function within the initial
    display data function.
    
    TODO:
    
    * Add a refresh icon to the menubar that will allow the user to
    manually call the update functions.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,6 +5,7 @@ var usersObj = {};
 $(document).ready(function() {
   populateObj();
   toggleUsers();
+  intervalUpdate();
 });
 
 /* Free Code Camp Twitch API pass-through to not utilize/publish API key
@@ -23,6 +24,7 @@ function populateObj() {
         usersObj[currentUser].username = currentUser;
         if (data.stream == null) {
           usersObj[currentUser].online = "offline";
+          usersObj[currentUser].game = "Offline";
         } else if (data.stream.hasOwnProperty("_id")) { //Tests if the user is streaming
           usersObj[currentUser].online = "online";
           usersObj[currentUser].game = data.stream.game;
@@ -42,7 +44,7 @@ function populateObj() {
                 break;
             }
           }
-          if (Object.keys(usersObj[currentUser]).length >= 3) {
+          if (Object.keys(usersObj[currentUser]).length = 4) {
             resolve(usersObj[currentUser]);
           }
         }); // End second API call
@@ -57,7 +59,6 @@ function populateObj() {
 
 function displayUsers(user) { // Populates interface with the data stored for each user when API calls populate values
   let logo = logoVerify(user.logo);
-  let game = gameVerify(user.game);
   let online = onlineStatus(user.online);
   let link = accountLink(user.game, user.username);
   $('.twitch-viewer').append('<div id="' + user.username + '" class="twitch-user ' + user.online + '">\
@@ -66,7 +67,7 @@ function displayUsers(user) { // Populates interface with the data stored for ea
       </div>\
     <div>\
       <p class="twitch-username">' + user.username + '</p>\
-      <p class="twitch-game">' + game + '</p>\
+      <p class="twitch-game">' + user.game + '</p>\
     </div>\
     <div class="icons">\
        ' + online + '\
@@ -81,15 +82,6 @@ function logoVerify(logoValue) {  // Verifies that logo does not contain null or
     return image = '<img class="twitch-avatar img-placeholder" />';
   } else {
     return image = '<img class="twitch-avatar" src=' + logoValue + ' />'
-  }
-}
-
-function gameVerify(gameValue) { // Removes undefined values to not display visually.
-  var game = "";
-  if (gameValue == undefined) {
-    return game = "Offline";
-  } else {
-    return game = gameValue;
   }
 }
 
@@ -110,6 +102,59 @@ function accountLink(game, username) { // Sets link of camera icon to account pa
     return link = '<a href="https://www.twitch.tv/' + username + '" target="_blank"><i class="fa fa-2x fa-video-camera"></i></a>';
   }
 }
+
+/* The following several functions are called in succession to update the online and game variables of each user.
+ * This will be called on a set interval and also available by the user with a refresh button.
+*/
+
+function intervalUpdate() {
+  setInterval(function() {
+    updateUsersObj();
+  }, 3000000);
+}
+
+function updateUsersObj() {
+  console.log("Interval successfully called");
+  for (var user in usersObj) {
+    /* Manually reset onlines and game values to be undefined before fetching the data
+     * from the API in order to not premauturely resolve the promise. This initial function filters out accounts that
+     * are unavailable or do not exist as no new information will be available for them. */
+    if (usersObj[user].game != "Account is unavailable" && usersObj[user].game != "Account does not exist") {
+      usersObj[user].online = undefined;
+      usersObj[user].game = undefined;
+      updateUserData(usersObj[user]);
+    }
+  }
+}
+
+function updateUserData(user) {
+  let userUpdatePromise = new Promise(function(resolve, reject) {
+    $.get("http://wind-bow.glitch.me/twitch-api/streams/" + user.username, function(data) {
+      if (data.stream == null) {
+        user.online = "offline";
+        user.game = "Offline";
+      } else if (data.stream.hasOwnProperty("_id")) { //Tests if the user is streaming
+        user.online = "online";
+        user.game = data.stream.game;
+      }
+      if (user.online != undefined && user.game != undefined) {
+          resolve(user);
+      }
+    }); // End API call
+  }); // End Promise
+  userUpdatePromise.then(function(userData) {
+    updateUserDisplay(userData);
+  });
+}
+
+function updateUserDisplay(user) {
+  let online = onlineStatus(user.online);
+  let game = user.game;
+  $('#' + user.username + '').find('.online-icon').replaceWith(online);
+  $('#' + user.username + '').find('.twitch-game').text(game);
+}
+
+// End user update functions
 
 function toggleUsers() { // Monitors clicks of menumar and shows/hides users accordingly. Online or offline class are on
   $('#all').on('click', function(e) {
